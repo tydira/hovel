6ed25dae5379e559532f57d1494ca884698323b8
Justin Krueger <justin@kroo.gs>
2016-07-26 05:13:58 -0600

    Include a wee readme and install script

---
+++ .bin/hovel-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Homebrew
+if [[ "`which brew`" == "" ]]; then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+brew update &&
+brew install node tmux tig vim weechat zsh tidy-html5 swiftlint &&
+brew upgrade &&
+
+# Python
+pip install --upgrade neovim flake8 &&
+pip3 install --upgrade neovim flake8 &&
+
+# Node
+npm update -g &&
+npm install -g coffee-script eslint babel-eslint eslint-plugin-react gulp tsd npm-check-updates &&
+npm upgrade -g &&
+
+# Vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
+vim +PlugInstall\! +GoUpdateBinaries +qa &&
+
+# Hovel
+alias hovel='git --git-dir=.hovel.git --work-tree=.' &&
+hovel config --local status.showUntrackedFiles no &&
+hovel submodule update --init
---
+++ README.md
@@ -0,0 +1,23 @@
+# hovel
+
+Dotfiles et al.
+
+## Deployment
+
+Clone to a bare repo in the current directory, likely $HOME:
+```shell
+git clone --bare git@github.com:kroogs/hovel.git .hovel.git
+```
+
+Checkout the contents of the bare repo:
+```shell
+git checkout --git-dir=.hovel.git --work-tree=.
+```
+> This will error if it attempts to overwrite files. Backup or remove the offending
+> files and run the command again. If you're sure it's safe, adding a ```-f``` switch
+> will overwrite them.
+
+Install desired tools:
+```shell
+source .bin/hovel-install.sh
+```
