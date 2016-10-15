b304d6e221a97a452ff46f0fe6432b1898510296
Justin Krueger <justin@kroo.gs>
2016-10-15 16:20:33 -0600

    Homebrew changed their install script path... Q.Q

--- .bin/hovel-install.sh
+++ .bin/hovel-install.sh
@@ -2,7 +2,7 @@
 
 # Homebrew
 if [[ "`which brew`" == "" ]]; then
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update &&
