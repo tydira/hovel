3737f890c5e7e5cec874121e988f6efce3bde8c2
Justin Krueger <justin@kroo.gs>
2016-12-17 08:08:59 -0700

    Remove this thing, gyah.

--- .hovel/setup.sh
+++
@@ -1,4 +0,0 @@
-alias hovel="git --git-dir=$HOME/.hovel/repo.git --work-tree=$HOME"
-hovel submodule update --init
-hovel update-index --assume-unchanged $HOME/README.md && rm $HOME/README.md
-source ~/.hovel/install.sh
