bf4d1c0e54d3c536ba075e76dfbf8ec0dbe9f55b
Justin Krueger <justin@kroo.gs>
2018-09-09 19:29:26 -0600

    Remove submodule used for omz

--- .hovel/install.sh
+++ .hovel/install.sh
@@ -3,7 +3,6 @@
 alias hovel="git --git-dir=$HOME/.hovel/repo.git --work-tree=$HOME"
 hovel config --local status.showUntrackedFiles no
 hovel update-index --assume-unchanged README.md && mv README.md ~/.hovel/ &&
-hovel submodule update --init --recursive &&
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
 nvim +PlugInstall +PlugUpdate +GoUpdateBinaries +qa
