07211097a13061770abfe4d29e5aa845545289dd
Justin Krueger <justin@kroo.gs>
2018-09-26 07:31:01 +0000

    Work more of the instructions into prepare.sh

--- .hovel/prepare.sh
+++ .hovel/prepare.sh
@@ -5,10 +5,12 @@ alias hovel="git --git-dir=$HOME/.hovel/repo.git --work-tree=$HOME"
 cd $HOME
 
 hovel config --local status.showUntrackedFiles no
-hovel update-index --assume-unchanged README.md
 hovel submodule update --init
-hovel checkout
+hovel checkout $*
 
 if [ -e README.md ]; then
+  hovel update-index --assume-unchanged README.md
   mv README.md ~/.hovel/
 fi
+
+chsh -s /usr/local/bin/fish
