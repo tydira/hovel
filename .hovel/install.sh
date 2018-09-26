ff3ede06843e58fa8e385c4e9853dfcf08743727
Justin Krueger <justin@kroo.gs>
2018-09-26 08:01:02 +0000

    Remove more detritus, clean up installation.

---
+++ .hovel/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+cd $HOME
+
+alias hovel="git --git-dir=.hovel/repo.git --work-tree=."
+
+hovel config --local status.showUntrackedFiles no
+hovel submodule update --init
+
+if [ -e README.md ]; then
+  hovel update-index --assume-unchanged README.md
+  mv README.md .hovel/
+fi
+
+os=`uname -s | awk '{print tolower($0)}'`
+install=$HOME/.hovel/install-$os.sh
+
+if [ -n $install ]; then
+  echo "Running $install" && sleep 3
+  sh $install
+
+  echo "Changing shell to /usr/local/bin/fish" && sleep 3
+  chsh -s /usr/local/bin/fish
+fi
