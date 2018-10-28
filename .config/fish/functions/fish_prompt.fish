4fda34d9e30e54b6ddafe69e5e0a653f51c6caf6
Justin Krueger <justin@kroo.gs>
2018-10-28 16:30:18 -0600

    Improve prompt display at /.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
 function fish_prompt
-  set prompt (basename (dirname $PWD))/(basename $PWD)
+  set prompt (echo $PWD | grep -Eo '([^/]+)?/([^/]+)?$')
 
   which arcus > /dev/null 2>&1
-  if test $status -a -n $fish_color_cwd_fade_to
+  if test "$status" -a -n "$fish_color_cwd_fade_to"
     echo -n $prompt | arcus $fish_color_cwd $fish_color_cwd_fade_to
   else
     set_color $fish_color_cwd
