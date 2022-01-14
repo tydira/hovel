1ceb48c7bd2eb606eaa2d955b9f27072b4e575fb
0x <0x@unio.wiki>
2022-01-14 12:25:04 -0800

    Remove unused packages

--- .hovel/install/node.sh
+++ .hovel/install/node.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 
-npm config set prefix $HOME/.local
 npm update -g
 npm install -g\
   npm\
+  pnpm\
   neovim\
   typescript\
-  yarn\
-  npm-check-updates\
-  eslint\
   prettier
