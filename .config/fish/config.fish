e350312aa355f69387b07da24abde69c68cdddd0
0x <0x@unio.wiki>
2022-01-14 11:46:36 -0800

    Change hovel alias to executable
    Also introduces a few fish config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,20 @@
 fish_vi_key_bindings
 
-set fish_user_paths $HOME/.cargo/bin $HOME/.local/bin
+set fish_user_paths $HOME/.local/bin /usr/local/opt/node*/bin /usr/local/opt/python/libexec/bin $HOME/.cargo/bin $HOME/.config/npm/bin $HOME/Library/Python/*/bin
+set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/library/
 
 set fish_term24bit 1
 set fish_greeting
 
 set -x EDITOR nvim
-set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src/
+set -x FZF_DEFAULT_COMMAND (which rg) --files
+set -x FZF_DEFAULT_OPTS "--height=50% --ansi --preview-window 'right:60%' --preview-window noborder --preview 'bat --theme=base16 --color=never --style=grid --line-range :300 {}'"
 
-alias hovel='git --git-dir=$HOME/.hovel/repo.git --work-tree=$HOME'
-
+alias f=fzf
 alias ls='exa'
 alias v='nvim'
 alias vs='nvim -S .session.vim'
-alias httpd='python -m SimpleHTTPServer 9000'
+alias httpd='python -m http.server 9000'
 alias mirror='wget --mirror --convert-links --adjust-extension --page-requisites --no-parent'
 
 alias g='git'
